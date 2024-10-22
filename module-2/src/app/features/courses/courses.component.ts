f6a6fb5e735eb7fefe2659290ddaa0227b4c9154
aditya_singh2 <aditya_singh2@epam.com>
2024-10-22 19:38:23 +0530

    updated

--- module-2/src/app/features/courses/courses.component.ts
+++ module-2/src/app/features/courses/courses.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { mockedAuthorsList, mockedCoursesList } from '@app/shared/mocks/mock';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent {
-  
+  courses = mockedCoursesList;
+  authors = mockedAuthorsList;
+
+  getAuthorsByIds(authorIds: string[]): string {
+    return this.authors
+      .filter(author => authorIds.includes(author.id))
+      .map(author => author.name)
+      .join(', ');
+  }
 }
--- module-2/src/app/shared/components/info/info.component.ts
+++ module-2/src/app/shared/components/info/info.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.scss']
 })
-export class InfoComponent {}
+export class InfoComponent {
+  @Input() title!: string; // Declare the input property for title
+  @Input() text!: string;  // Declare the input property for text
+}
 // Use the names `title` and `text`.
